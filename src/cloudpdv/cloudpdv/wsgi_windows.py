f726d702f4c8f0ce5d04216fa58b029924a140e5
angelmx777 <amgmx777@gmail.com>
2023-01-07 18:31:41 -0600

    se agrega wsgi_windows.py para usarse en apache django windows en produccion

---
+++ src/cloudpdv/cloudpdv/wsgi_windows.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import site
+
+# Add the site-packages of the chosen virtualenv to work with
+#site.addsitedir('C:/Users/myuser/Envs/my_application/Lib/site-packages')
+site.addsitedir('C:/Python37/lib/site-packages/')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('C:/dev/sitiosweb/cloudpdv/src/cloudpdv')
+sys.path.append('C:/dev/sitiosweb//cloudpdv/src/cloudpdv/cloudpdv')
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cloudpdv.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloudpdv.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
